de75be93fb9487e767252db2c7cb5fa2658ecc45
tuan dao <tuan.dao@sparkxlab.com>
2018-04-05 15:35:11 +0700

    Make changes according to thienhv's review

--- app/serializers/developer_serializer.rb
+++ app/serializers/developer_serializer.rb
@@ -1,5 +1,5 @@
 class DeveloperSerializer < ActiveModel::Serializer
-    attributes :id, :email, :languages, :programming_languages
-    has_many :languages, serializer: LanguageSerializer
-    has_many :programming_languages, serializer: ProgrammingLanguageSerializer
+    attributes :id, :email
+    has_many :languages
+    has_many :programming_languages
 end
