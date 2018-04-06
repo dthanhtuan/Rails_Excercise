875ddb04071b35935836fa817b69fe27eadf5dc4
tuan dao <tuan.dao@sparkxlab.com>
2018-04-06 19:28:17 +0700

    correct condition for 'show' method

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -4,6 +4,7 @@ module Api
       before_action :set_developer, only: %i[show]
 
       def show
+        return unless stale?(last_modified: @developer.updated_at.utc)
         cache_options = { compress: true, expires_in: 24.hours, race_condition_ttl: 10 }
         Rails.cache.fetch("#{@developer.cache_key}/#{__method__}", cache_options) do
           render json: @developer, status: :ok
