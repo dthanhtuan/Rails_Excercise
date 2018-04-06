04080d676e52a0cdd35173121cade14e6d66a16b
tuan dao <tuan.dao@sparkxlab.com>
2018-04-06 18:57:58 +0700

    Remove unnecessary code in performance_spec.rb

--- spec/requests/v1/performance_spec.rb
+++ spec/requests/v1/performance_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
-require 'support/shared_context_for_api.rb'
 
 describe 'Performance testing' do
   context '10 requests at the same time and 10 times in a row' do
-    let(:response_data) do
-      {
-        email: @developer.email,
-        languages: @developer.languages.to_json,
-        progamming_languages: @developer.programming_languages.to_json
-      }
-    end
-
     it 'returns 200 with JSON' do
       expect do
         10.times do
